1d40dc804a37fcf3fc9bd48fc63d6efa47228cba
Jessica L Lynch <jely2159@colorado.edu>
2021-02-18 23:04:21 -0700

    start of backend with Express.js
    test backend with simple get

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,21 @@
+/*eslint-env es2021*/
+//the above comment fixes Eslint Parsing error: The keyword 'const' is reserved
+
 console.log('I am a web app!');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors()) //if cors enabled then you need to have really great security for your app!
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world!'
+  })
+})
+
+app.listen(process.env.PORT || 8080)
